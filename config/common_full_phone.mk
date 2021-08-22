358083c0ca966ff6dd1a35a394ed61436deddb45
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-08-22 10:25:07 +0530

    Update common_full_phone.mk

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,11 +1,11 @@
-# Inherit full common ShadowOS stuff
-$(call inherit-product, vendor/shadow/config/common_full.mk)
+# Inherit full common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
 
 # Include Lineage LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/shadow/overlay/dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 
-$(call inherit-product, vendor/shadow/config/telephony.mk)
+$(call inherit-product, vendor/lineage/config/telephony.mk)
